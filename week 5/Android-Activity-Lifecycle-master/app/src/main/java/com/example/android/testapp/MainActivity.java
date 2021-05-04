f71ce4032e0ef7fb0c20c636b14cef1308210201
usman724 <83121116+sdmobile708@users.noreply.github.com>
2021-05-04 23:13:08 +0500

    new commit
    new commit

---
+++ week 5/Android-Activity-Lifecycle-master/app/src/main/java/com/example/android/testapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.testapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("onCreate ------ ", "MainActivity: onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("onStart ------ ","MainActivity: onStart()");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("onResume ------ ","MainActivity: onResume()");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("onPause ------ ","MainActivity: onPause()");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("onStop ------ ","MainActivity: onStop()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("onDestroy ------ ","MainActivity: onDestroy()");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("onRestart ------ ","MainActivity: onRestart()");
+
+    }
+
+    public void switchActivity(View view){
+        Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
+        startActivity(intent);
+    }
+}
